fb8b2faa18e5f71e6ca69de0f491f8110f39701c
Chad Stark <chad.d.stark@hotmail.com>
2021-08-19 15:22:10 -0700

    set up some sections for content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
+import _ from "lodash";
 
+function renderTimes(num) {
+  let card = [];
+  _.times(num, () => {
+    card.push(
+      <section>
+        <h1>Nice Curves!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          lobortis turpis dui, vel vulputate nunc sollicitudin et. Aenean
+          sollicitudin orci tempus, semper nibh vel, porttitor quam. Orci varius
+          natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Praesent bibendum purus ut interdum elementum. Aenean quis
+          pulvinar magna. Integer sed vestibulum dui. Ut tempor pretium ornare.
+          Mauris tortor purus, pulvinar sed lectus sed, cursus pellentesque
+          arcu. Pellentesque consectetur iaculis nulla, ac sagittis metus
+          molestie nec.
+        </p>
+      </section>
+    );
+  });
+  return card;
+}
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{renderTimes(8)}</div>;
 }
 
 export default App;
